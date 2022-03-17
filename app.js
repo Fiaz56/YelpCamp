7abb185fec034a8ef0c4e639d60f1fe19de838a2
Fiaz56 <mdfayyu786@gmail.com>
2022-03-18 01:47:27 +0530

    add start script

--- app.js
+++ app.js
@@ -144,6 +144,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-    console.log("Serving at Port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving at Port ${port}`);
 });
